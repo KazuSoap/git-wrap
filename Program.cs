2fe19f58d7d450e88f88c1e4c140321c9ebf0664
khamada <k-hamada@k-src.jp>
2025-04-24 18:17:52 +0900

    外部プロセス実行部をクラス化

--- Program.cs
+++ Program.cs
@@ -1,26 +1,21 @@
 ﻿using System.Diagnostics;
+using System.IO.Pipes;
 using System.Text;
 using System.Text.RegularExpressions;
 
 internal class Program
 {
-    const string MSYS2_root = @"C:\msys64";
-    static readonly string MSYS2_bin = Path.Combine(MSYS2_root, "usr", "bin");
-    static readonly string MSYS2_home = Environment.GetEnvironmentVariable("HOME") ?? Path.Combine(MSYS2_root, "home", Environment.GetEnvironmentVariable("USERNAME") ?? "");
-    static readonly string envPath = $"{MSYS2_bin};{Environment.GetEnvironmentVariable("PATH") ?? ""}";
-
     private static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.GetEncoding("utf-8");
 
-        var gitProcInfo = new ProcessStartInfo(Path.Combine(MSYS2_bin, "git"));
-        foreach (var arg in args)
+        // git コマンドの実行
+        var gitProc = new SubProc("git")
         {
-            // 中括弧はエスケープ必要
-            gitProcInfo.ArgumentList.Add(arg.Replace(@"{", @"\{").Replace(@"}", @"\}"));
-        }
+            ArgumentList = args
+        };
 
-        int exitCode = ExecSubProc(gitProcInfo, out string gitOut);
+        int exitCode = gitProc.Exec(out string gitOut);
         if (exitCode != 0)
         {
             Environment.Exit(exitCode);
@@ -32,14 +27,16 @@ internal class Program
             Environment.Exit(0);
         }
 
-        var cygpathProcInfo = new ProcessStartInfo(Path.Combine(MSYS2_bin, "cygpath"));
+        // msys2 git では一部コマンド出力のパス形式が unix 形式 のため、
+        // cygpath を使って windows 形式のパスに変換する
+        var cygpathProc = new SubProc("cygpath");
         var gitOutElems = gitOut.Split(" ", StringSplitOptions.RemoveEmptyEntries);
         try
         {
             var gitOutFixed = string.Join(" ", gitOutElems.Select(elem =>
             {
-                cygpathProcInfo.Arguments = $"-w {elem}";
-                return ExecSubProc(cygpathProcInfo, out var cygpathOut) == 0 ? cygpathOut : throw new Exception();
+                cygpathProc.Arguments = $"-w {elem}";
+                return cygpathProc.Exec(out var cygpathOut) == 0 ? cygpathOut : throw new Exception();
             }));
 
             Console.Write(gitOutFixed);
@@ -51,18 +48,53 @@ internal class Program
 
         Environment.Exit(0);
     }
+}
 
-    static int ExecSubProc(ProcessStartInfo psi, out string stdout)
+internal class SubProc
+{
+    const string MSYS2_root = @"C:\msys64";
+    static readonly string MSYS2_bin = Path.Combine(MSYS2_root, "usr", "bin");
+    static readonly string MSYS2_home = Environment.GetEnvironmentVariable("HOME") ?? Path.Combine(MSYS2_root, "home", Environment.GetEnvironmentVariable("USERNAME") ?? "");
+    static readonly string envPath = $"{MSYS2_bin};{Environment.GetEnvironmentVariable("PATH") ?? ""}";
+    private readonly ProcessStartInfo psi;
+
+    public string[] ArgumentList
     {
-        psi.UseShellExecute = false;
-        psi.RedirectStandardOutput = true;
-        psi.RedirectStandardError = true;
-        psi.CreateNoWindow = true;
+        set
+        {
+            foreach (var arg in value)
+            {
+                // 中括弧はエスケープ必要
+                psi.ArgumentList.Add(arg.Replace(@"{", @"\{").Replace(@"}", @"\}"));
+            }
+        }
+    }
+
+    public string Arguments
+    {
+        set
+        {
+            psi.Arguments = value;
+        }
+    }
+
+    public SubProc(string executableFileName)
+    {
+        psi = new(Path.Combine(MSYS2_bin, executableFileName))
+        {
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+        };
         psi.EnvironmentVariables["PATH"] = envPath;
         psi.EnvironmentVariables.Add("HOME", MSYS2_home);
+    }
+
+    public int Exec(out string stdout)
+    {
         stdout = "";
         var stderr = "";
-
         try
         {
             using var proc = Process.Start(psi);
@@ -74,11 +106,9 @@ internal class Program
             stdout = proc.StandardOutput.ReadToEnd();
             stderr = proc.StandardError.ReadToEnd();
 
-            if (!proc.WaitForExit(10 * 1000))
-            {
-                proc.Kill();
-                return 1;
-            }
+            // git & cygpath コマンドはハングしないという希望的観測により
+            // タイムアウトを設定せず終了を待つ
+            proc.WaitForExit();
             return proc.ExitCode;
         }
         catch (Exception ex)
